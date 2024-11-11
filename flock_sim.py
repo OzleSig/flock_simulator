919f17206fd4019681e6dd7e404a0440f8cf0f04
OzleSig <ozlemsigbeku@protonmail.com>
2024-11-11 19:33:55 +0000

    minor changes

--- flock_sim.py
+++ flock_sim.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
-import time
 pygame.init()
+import time
 from random import randint
 
 windowsize = 800
@@ -13,16 +13,24 @@ t1 = 5
 t2 = 15
 t3 = 10
 time1 = time.time()
+wait_time = 10 #in seconds
 boids = 20
 boids_pos = []
 for x in range(boids):
     boids_pos.append([randint(0, windowsize), randint(0, windowsize)])
 
-def display():
+def render():
     surface.fill(white)
     for x in range(boids):
         draw_boid(boids_pos[x][0],boids_pos[x][1])
-    pygame.display.flip()
+        pygame.display.flip()
+
+def update():
+        for x in boids_pos:
+            if x[0] > windowsize:
+                x[0] = 0
+            else:
+                x[0]+=1
 
 def draw_boid(x,y):
     pygame.draw.line(surface, black, (x,y), (x+t2,y+t1))
@@ -34,14 +42,12 @@ def flock_loop():
     global boids
     global time1
     while run:
-        display()
-        for x in boids_pos:
-                if x[0] > windowsize:
-                    x[0] = 0
-                else:
-                    x[0]+=1
-        clock = pygame.time.Clock()
-        print(clock.tick(50))
+        update()
+        time2 = time.time()
+        print("ooo")
+        if (time2 - time1)%10 == 0:
+            print("hi")
+            render()
         for event in pygame.event.get():
             if event.type ==pygame.QUIT:
                 run = False
