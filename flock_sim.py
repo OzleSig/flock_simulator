948e7d17a57df45d1f5c920051ba0cead9f420ae
OzleSig <ozlemsigbeku@protonmail.com>
2024-11-17 20:30:28 +0000

    updated movement and positioning

--- flock_sim.py
+++ flock_sim.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 pygame.init()
 import time
+import math
 from random import randint
 
 windowsize = 800
@@ -9,37 +10,58 @@ white = (255,255,255)
 black = (0,0,0)
 surface = pygame.display.set_mode((windowsize,windowsize))
 run = True
-t1 = 5
-t2 = 15
-t3 = 10
-wait_time = 0.5 #in seconds
+wait_time = 1/60 #in seconds
 boids = 20
 boids_pos = []
-for x in range(boids):
-    boids_pos.append([randint(0, windowsize), randint(0, windowsize)])
+unit_vectors = []
+vector_mag = 10
+
+def vector(x,y):
+    magnitude = math.sqrt((x*x)+(y*y))
+    x = x/magnitude
+    y = y/magnitude
+    return [x,y]
+
+def vector_adj():
+    global boids_pos
+    for i in range(boids):
+        boids_pos.append([randint(-windowsize, windowsize), randint(-windowsize, windowsize)])
+        x = boids_pos[i][0]
+        y = boids_pos[i][1]
+        boids_pos[i] = vector(x,y)
+        unit_vectors.append(boids_pos[i])
+        x_adj = randint(0, windowsize)
+        y_adj = randint(0, windowsize)
+        x = x_adj + boids_pos[i][0]*vector_mag
+        y = y_adj + boids_pos[i][1]*vector_mag
+        boids_pos[i] = [x,y]
 
 def render():
     surface.fill(white)
-    for x in range(boids):
-        draw_boid(boids_pos[x][0],boids_pos[x][1])
-        pygame.display.flip()
+    draw_boid()
+    pygame.display.flip()
 
 def update():
-        for x in boids_pos:
-            if x[0] > windowsize:
-                x[0] = 0
-            else:
-                x[0]+=1
+    for i, coord in enumerate(boids_pos):
+        coord[0] = coord[0] % windowsize
+        coord[1] = coord[1] % windowsize
+        coord[0]+= unit_vectors[i][0]*0.001
+        coord[1]+= unit_vectors[i][1]*0.001
+                
 
-def draw_boid(x,y):
-    pygame.draw.line(surface, black, (x,y), (x+t2,y+t1))
-    pygame.draw.line(surface, black, (x,y+t3), (x+t2,y+t1))
-    pygame.draw.line(surface, black, (x,y), (x,y+t3))
+def draw_boid():
+    for i, unit_v in enumerate(boids_pos):
+        x = unit_v[0]
+        y = unit_v[1]
+        x_adj = x + unit_vectors[i][0]*vector_mag
+        y_adj = y + unit_vectors[i][1]*vector_mag
+        pygame.draw.line(surface, black, (x_adj,y_adj), (x,y))
 
 def flock_loop():
     global run
     global boids
     time1 = time.time()
+    vector_adj()
     while run:
         update()
         time2 = time.time()
@@ -50,3 +72,4 @@ def flock_loop():
             if event.type ==pygame.QUIT:
                 run = False
 flock_loop()
+print(boids_pos)
